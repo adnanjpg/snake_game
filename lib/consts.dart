3a73367ab8c87c4a15b0033e24013d4c6d2a765e
Adnan <adnanfahedhr@hotmail.com>
2021-08-08 09:31:12 +0300

    calmer colors for development

--- lib/consts.dart
+++ lib/consts.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 
 const double size = 30, borderWidth = 1;
 int boardSizeX = 12, boardSizeY = 12;
-const Color bgCubeColor = Colors.green,
-    snakeCubeColor = Colors.red,
-    snakeHeadColor = Colors.blue,
-    dangerAreaColor = Colors.red;
+const Color bgCubeColor = Color(0xFF123456),
+    snakeCubeColor = Color(0xFF333333),
+    snakeHeadColor = Colors.black,
+    dangerAreaColor = Color(0xFF37474F);
+// const Color bgCubeColor = Colors.green,
+//     snakeCubeColor = Colors.red,
+//     snakeHeadColor = Colors.blue,
+//     dangerAreaColor = Colors.red;
 const initSnakeSize = 6;
 
 int speed = 5;
