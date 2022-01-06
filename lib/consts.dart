56ff516631740d148710107c0e20957ca190ac0b
Adnan <adnanfahedhr@gmail.com>
2022-01-06 16:18:49 +0300

    reinit color scheme

--- lib/consts.dart
+++ lib/consts.dart
@@ -2,14 +2,12 @@ import 'package:flutter/material.dart';
 
 const double size = 30, borderWidth = 1;
 int boardSizeX = 12, boardSizeY = 12;
-const Color bgCubeColor = Color(0xFF123456),
-    snakeCubeColor = Color(0xFF333333),
-    snakeHeadColor = Colors.black,
+
+const Color bgCubeColor = Color(0xFF4C3F91),
+    snakeCubeColor = Color(0xFF9145B6),
+    snakeHeadColor = Color(0xFFB958A5),
     dangerAreaColor = Color(0xFF37474F);
-// const Color bgCubeColor = Colors.green,
-//     snakeCubeColor = Colors.red,
-//     snakeHeadColor = Colors.blue,
-//     dangerAreaColor = Colors.red;
+
 const initSnakeSize = 6;
 
 int speed = 5;
